46c21045da6fc45d3b5cc6704d510b8e6c707bb0
iamfutureflash <xenode2000@gmail.com>
2022-10-13 19:04:10 +0530

    added max min program in POD 01 finally solved after doing debug

--- java_tutorials/src/GFG/ProblemOfTheDay/_01_Thu_Oct_13_MaxMin.java
+++ java_tutorials/src/GFG/ProblemOfTheDay/_01_Thu_Oct_13_MaxMin.java
@@ -1,4 +1,28 @@
 package GFG.ProblemOfTheDay;
+//link of the problem https://practice.geeksforgeeks.org/problems/max-min/1
 
 public class _01_Thu_Oct_13_MaxMin {
+    public static void main(String[] args) {
+       int A[] = {1,2,3,-3};
+       int N=A.length;
+        System.out.println(findSum(A,N));
+    }
+    public static int findSum(int A[],int N){
+        //code here
+        // for min and max
+        int min =0;
+        int max =0;
+        for(int i=0;i<N;i++){
+            if(A[i]<min){
+                min = A[i];
+            }
+            if(A[i]>max){
+                max = A[i];
+            }
+        }
+        System.out.println(min + " " +  max);
+        int sum = min + max;
+        return sum;
+
+    }
 }
