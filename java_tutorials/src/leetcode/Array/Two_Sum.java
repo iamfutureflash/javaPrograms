22fb221928cd2c58eb5d9d1a36421bd082cf078d
iamfutureflash <xenode2000@gmail.com>
2022-10-15 21:16:57 +0530

    Solved Two Sum using array takes 50 to 60 ms 49.61% faster then other https://leetcode.com/problems/two-sum/submissions/

---
+++ java_tutorials/src/leetcode/Array/Two_Sum.java
@@ -0,0 +1,29 @@
+package leetcode.Array;
+
+public class Two_Sum {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int target = 4;
+        int[] result = new int[2];
+        result = twoSum(nums,target);
+        for (int i = 0; i < result.length ; i++) {
+            System.out.println(result[i]);
+        }
+    }
+        public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        int temp_value=0;
+            for (int i = 0; i <nums.length ; i++) {
+                temp_value = nums[i];
+                for (int j = i+1; j < nums.length ; j++) {
+                    if((temp_value + nums[j])==target){
+                        result[0] = i;
+                        result[1] = j;
+                    }
+                }
+            }
+
+        return result;
+        }
+    }
+
